3c4054ac1d9081b08ea45105c56b510754fc449e
Peter <PeterC499@deltalearns.ca>
2020-09-30 12:18:50 -0700

    ggg

--- problem1.py
+++ problem1.py
@@ -18,10 +18,10 @@ Access granted
 Acces denied
 """
 i= 0
+user= input("what is your Username ").strip()
+pas = input("what is your password ").strip()
 while i < 3:
     i += 1
-    user= input("what is your Username ").strip()
-    pas = input("what is your password ").strip()
     if user != "admin" or pas != "12345":
         print("Access denied.")
     if user == "admin" and pas == "12345":
